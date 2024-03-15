6818701b9365a48f3cd9f8abcd7bec60eea857fc
Brunx96 <160648683+Brunx96@users.noreply.github.com>
2024-03-15 17:38:34 +0000

    Desafio JavaScript

---
+++ desafio.js
@@ -0,0 +1,15 @@
+let nota1 = 10;
+let nota2 = 8;
+let nota3 = 6;
+
+let media = (nota1 + nota2 + nota3) / 3;
+
+if (media >= 7) {
+    console.log ("Aluno aprovado!");
+} else if (media <=6.9) {
+    console.log ("Aluno reprovado!");
+} else {
+    console.log ("Aluno ficou de recuperação");
+}
+
+
